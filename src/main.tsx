ec37e9ae8d1e8c2c61e7a48e4812f4b6072ca2d1
Srujan180 <annamsrujanreddy2002@gmail.com>
2025-09-03 12:03:28 +0530

    stritmode fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "./App.css"
 createRoot(document.getElementById('root')!).render(
-
+<StrictMode>
     <App/>
+</StrictMode>
 
 )
